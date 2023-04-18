3a800455a6ec828334dc2ff9ffef51b3b79dd19c
Charles Morin <charles.mor@outlook.fr>
2023-04-18 16:22:55 -0400

    Added token and token env var option

--- bin/cli.ts
+++ bin/cli.ts
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 
 import { cli } from "substreams-sink";
-import { action } from "../index.js"
+import { action, DEFAULT_DISCORD_API_TOKEN_ENV } from "../index.js"
 import pkg from "../package.json";
 
 const program = cli.program(pkg);
 const command = cli.run(program, pkg);
+
+command.option('--discord-api-token <string>', 'API token for the Discord bot')
+command.option('--discord-api-token-envvar <string>', 'Environnement variable name of the API token for the Discord bot', DEFAULT_DISCORD_API_TOKEN_ENV)
 
 command.action(action);
 program.parse();
